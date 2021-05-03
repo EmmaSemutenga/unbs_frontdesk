d37b43519c18dd9d11c0b937945112c2641f1407
Emmanuel Semutenga <emzsema@gmail.com>
2021-05-03 13:21:11 +0300

    added secret key

--- unbssite/settings.py
+++ unbssite/settings.py
@@ -11,20 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(dotenv_path=BASE_DIR / '.env')
+# load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-kop6*6+dg9a-90j_83d!r6)p=-@ub%wux&g6@bzh2e)=-(-94a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
