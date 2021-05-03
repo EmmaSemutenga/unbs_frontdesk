8123b9936cca46fa12073a081205e5ad8fddd23d
Emmanuel Semutenga <emzsema@gmail.com>
2021-05-03 15:53:31 +0300

    Added gender field

--- customer/models.py
+++ customer/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    company_name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    contact_person = models.CharField(max_length=100)
-    telephone_contact = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=100, null=True)
+    contact_person = models.CharField(max_length=100, null=True)
+    GENDER_CHOICES = (
+        ("Male", "Male"),
+        ("Female", "Female"),
+    )
+    gender = models.CharField(max_length=20, choices = GENDER_CHOICES, null=True)
+    telephone_contact = models.CharField(max_length=100, null=True)
     DEPARTMENT_CHOICES = (
         ("Testing", "Testing"),
         ("Certification", "Certification"),
@@ -16,10 +21,10 @@ class Customer(models.Model):
         ("Office of the Executive Director", "Office of the Executive Director")
     )
 
-    department_to_vist = models.CharField(max_length=100, choices = DEPARTMENT_CHOICES)
-    email = models.EmailField()
-    visit_date = models.DateTimeField(auto_now_add=True)
-    first_time_customer = models.BooleanField(default=False)
+    department_to_vist = models.CharField(max_length=100, choices = DEPARTMENT_CHOICES, null=True)
+    email = models.EmailField(null=True)
+    visit_date = models.DateTimeField(auto_now_add=True, null=True)
+    first_time_customer = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.company_name
